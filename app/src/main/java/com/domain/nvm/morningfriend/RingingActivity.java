32ec3d7d99c5cce78ba3e6f5d57083a55a0a465d
Never M <nevermournee@gmail.com>
2016-06-21 17:19:03 +0300

    Play audio in background, stop

--- app/src/main/java/com/domain/nvm/morningfriend/RingingActivity.java
+++ app/src/main/java/com/domain/nvm/morningfriend/RingingActivity.java
@@ -1,15 +1,69 @@
 package com.domain.nvm.morningfriend;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class RingingActivity extends AppCompatActivity {
 
+    private Button mStopButton;
+    private RingingService mService;
+    private boolean mBound = false;
+
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            RingingService.RingingBinder binder = (RingingService.RingingBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ringing);
+
+        mStopButton = (Button) findViewById(R.id.button_ringing_stop);
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mService.stopPlaying();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, RingingService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        // this won't create another instance of service, but will call onStartCommand
+        startService(intent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 }
---
+++ app/src/main/java/com/domain/nvm/morningfriend/RingingService.java
@@ -0,0 +1,59 @@
+package com.domain.nvm.morningfriend;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class RingingService extends Service {
+
+    private final IBinder mBinder = new RingingBinder();
+
+    private boolean isPlaying;
+    private MediaPlayer mp;
+
+    public class RingingBinder extends Binder {
+        RingingService getService() {
+            return RingingService.this;
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startPlaying();
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mp = MediaPlayer.create(this, R.raw.eh);
+        mp.setLooping(true);
+    }
+
+    public void startPlaying() {
+        if (!isPlaying) {
+            isPlaying = true;
+            mp.start();
+        }
+    }
+
+    public void stopPlaying() {
+        isPlaying = false;
+        mp.stop();
+        stopSelf();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.release();
+    }
+}

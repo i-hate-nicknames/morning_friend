a26d2c993a576cad422baecefe0158798c32e93b
Never M <nevermournee@gmail.com>
2016-07-05 12:30:30 +0300

    Add utils tests and update utils code

--- app/src/main/java/com/domain/nvm/morningfriend/untangle/Utils.java
+++ app/src/main/java/com/domain/nvm/morningfriend/untangle/Utils.java
@@ -1,6 +1,7 @@
 package com.domain.nvm.morningfriend.untangle;
 
 import android.graphics.PointF;
+import android.util.Log;
 
 public class Utils {
 
@@ -14,10 +15,10 @@ public class Utils {
         return Math.abs(x-y) < ALMOST_EQUAL_EPSILON;
     }
 
-    public static float slope(PointF a1, PointF a2) {
+    public static float slope(float ax, float ay, float bx, float by) {
         float slope;
-        if (a2.x - a1.x == 0) {
-            if (a2.y >= a1.y) {
+        if (bx - ax == 0) {
+            if (by >= ay) {
                 slope = Float.POSITIVE_INFINITY;
             }
             else {
@@ -25,16 +26,16 @@ public class Utils {
             }
         }
         else {
-            slope = (a2.y - a1.y) / (a2.x - a1.x);
+            slope = (by - ay) / (bx - ax);
         }
         return slope;
     }
 
-    public static float intercept(PointF a, float slope) {
+    public static float intercept(float px, float py, float slope) {
         if (slope == Float.POSITIVE_INFINITY || slope == Float.NEGATIVE_INFINITY) {
-            return a.x;
+            return px;
         }
-        return a.y - a.x * slope;
+        return py - px * slope;
     }
 
     /**
@@ -49,33 +50,39 @@ public class Utils {
     }
 
     public static boolean intersects(PointF a1, PointF a2, PointF b1, PointF b2) {
-        float slopeA = slope(a1, a2), slopeB = slope(b1, b2);
-        float interA = intercept(a1, slopeA);
-        float interB = intercept(b1, slopeB);
+        return intersects(a1.x, a1.y, a2.x, a2.y, b1.x, b1.y, b2.x, b2.y);
+    }
+
+    public static boolean intersects(float a1x, float a1y, float a2x, float a2y,
+                                     float b1x, float b1y, float b2x, float b2y) {
+        float slopeA = slope(a1x, a1y, a2x, a2y);
+        float slopeB = slope(b1x, b1y, b2x, b2y);
+        float interA = intercept(a1x, a1y, slopeA);
+        float interB = intercept(b1x, b1y, slopeB);
         boolean isAVertical =
                 slopeA == Double.POSITIVE_INFINITY || slopeA == Double.NEGATIVE_INFINITY;
         boolean isBVertical =
                 slopeB == Double.POSITIVE_INFINITY || slopeB == Double.NEGATIVE_INFINITY;
         if (isAVertical && isBVertical) {
-            if (a1.x != b1.x) return false;
-            return !(liesWithin(b1.y, a1.y, a2.y) || liesWithin(b2.y, a1.y, a2.y));
+            if (a1x != b1x) return false;
+            return liesWithin(b1y, a1y, a2y) || liesWithin(b2y, a1y, a2y);
         }
         if (isAVertical) {
             float sectY = interB + slopeB * interA;
             // belongs to both A and B
-            return liesWithin(sectY, a1.y, a2.y) && liesWithin(interA, b1.x, b2.x);
+            return liesWithin(sectY, a1y, a2y) && liesWithin(interA, b1x, b2x);
         }
         if (isBVertical) {
             float sectY = interA + slopeA * interB;
-            return liesWithin(sectY, b1.y, b2.y) && liesWithin(interB, a1.x, a2.x);
+            return liesWithin(sectY, b1y, b2y) && liesWithin(interB, a1x, a2x);
         }
         // two lines are parallel
         if (almostEqual(slopeA, slopeB)) {
             // lines overlap and have same intercept
-            return almostEqual(interA, interB) && segmentsOverlap(a1.x, a2.x, b1.x, b2.x);
+            return almostEqual(interA, interB) && segmentsOverlap(a1x, a2x, b1x, b2x);
         }
         float sectX = -(interA-interB)/slopeA-slopeB;
-        return liesWithin(sectX, a1.x, a2.x) && liesWithin(sectX, b1.x, b2.x);
+        return liesWithin(sectX, a1x, a2x) && liesWithin(sectX, b1x, b2x);
 
     }
 }

4d3e538fbbb40b1f45b966dcc77e8a885a9324ae
Never M <nevermournee@gmail.com>
2016-07-20 16:58:50 +0300

    Use service for scheduling on boot

--- app/src/main/java/com/domain/nvm/morningfriend/AlarmRegisterReceiver.java
+++ app/src/main/java/com/domain/nvm/morningfriend/AlarmRegisterReceiver.java
@@ -6,12 +6,11 @@ import android.content.Intent;
 
 import java.util.Date;
 
-public class BootReceiver extends BroadcastReceiver {
+public class AlarmRegisterReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        boolean isOn = AlarmScheduler.isEnabled(context);
-        Date time = AlarmScheduler.getAlarmTime(context);
-        AlarmScheduler.setRingingAlarm(context, time, isOn);
+        Intent serviceIntent = new Intent(context, AlarmRegisterService.class);
+        context.startService(serviceIntent);
     }
 }
---
+++ app/src/main/java/com/domain/nvm/morningfriend/AlarmRegisterService.java
@@ -0,0 +1,26 @@
+package com.domain.nvm.morningfriend;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import java.util.Date;
+
+public class AlarmRegisterService extends Service {
+    public AlarmRegisterService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // TODO: find the soonest alarm and schedule it
+        boolean isOn = AlarmScheduler.isEnabled(getApplicationContext());
+        Date time = AlarmScheduler.getAlarmTime(getApplicationContext());
+        AlarmScheduler.setRingingAlarm(getApplicationContext(), time, isOn);
+        return START_NOT_STICKY;
+    }
+}

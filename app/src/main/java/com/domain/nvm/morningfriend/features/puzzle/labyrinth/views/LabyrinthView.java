f5b5a8e5319ab4264d0bb6fa8b9cbe70cb2eabed
Never M <nevermournee@gmail.com>
2017-02-02 13:39:10 +0200

    Center labyrinth and render walls properly

--- app/src/main/java/com/domain/nvm/morningfriend/features/puzzle/labyrinth/views/LabyrinthView.java
+++ app/src/main/java/com/domain/nvm/morningfriend/features/puzzle/labyrinth/views/LabyrinthView.java
@@ -13,19 +13,23 @@ import com.domain.nvm.morningfriend.features.puzzle.labyrinth.data.Labyrinth;
 
 public class LabyrinthView extends View implements Puzzle {
 
+    private static final int PUZZLE_OFFSET = 50;
+
     private Bitmap labyrinthImg;
     private Labyrinth labyrinth;
     private PuzzleHost puzzleHost;
-    private int width, height, tileSize, wallWidth;
+    private int puzzleX;
+    private int puzzleY;
 
     @Override
     public void init(Difficulty difficulty) {
-        width = getWidth();
-        height = getHeight();
-        generateLabyrinth();
-        tileSize = width / labyrinth.getSize();
-        wallWidth = (tileSize < 10) ? 1 : tileSize / 10;
-        generateImage();
+        int mainWidth = getWidth();
+        int mainHeight = getHeight();
+        labyrinth = generateLabyrinth();
+        int puzzleWidth = mainWidth - PUZZLE_OFFSET*2;
+        puzzleX = PUZZLE_OFFSET;
+        puzzleY = (mainHeight - puzzleWidth) / 2;
+        labyrinthImg = generateImage(puzzleWidth, labyrinth);
     }
 
     @Override
@@ -45,42 +49,49 @@ public class LabyrinthView extends View implements Puzzle {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(labyrinthImg, 0, 0, null);
+        canvas.drawBitmap(labyrinthImg, puzzleX, puzzleY, null);
         // draw player position
     }
 
-    private void generateLabyrinth() {
-        labyrinth = new Labyrinth(5);
+    private Labyrinth generateLabyrinth() {
+        return new Labyrinth(5);
     }
 
-    private void generateImage() {
+    private Bitmap generateImage(int width, Labyrinth lab) {
+        int tileSize = width / labyrinth.getSize();
+        int wallWidth = (tileSize < 10) ? 1 : tileSize / 10;
+        // coordinates of leftmost topmost tile
+        int originX = wallWidth, originY = wallWidth;
+        int canvasWidth = width+wallWidth, canvasHeight = width+wallWidth;
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-        labyrinthImg = Bitmap.createBitmap(width, height, conf);
+        Bitmap image = Bitmap.createBitmap(canvasWidth, canvasHeight, conf);
         Canvas c = new Canvas();
-        c.setBitmap(labyrinthImg);
+        c.setBitmap(image);
         Paint wallPaint = new Paint();
         wallPaint.setColor(Color.BLACK);
-        wallPaint.setStrokeWidth(wallWidth);
         Paint tilePaint = new Paint();
         tilePaint.setColor(Color.LTGRAY);
+
         c.drawPaint(tilePaint);
-        int wallOffset = wallWidth/2;
-        // draw top and left walls
-        c.drawLine(0, 0, width, 0, wallPaint);
-        c.drawLine(0, 0, 0, height, wallPaint);
+        // draw top and left walls outside of tiles
+        // todo: draw the entrance to the labyrinth
+        c.drawRect(0, 0, canvasWidth, wallWidth, wallPaint);
+        c.drawRect(0, 0, wallWidth, canvasHeight, wallPaint);
+        // draw all right and bottom walls so that they would occupy tiles' space
         for (int i = 0; i < labyrinth.getSize(); i++) {
             for (int j = 0; j < labyrinth.getSize(); j++) {
-                int x1 = tileSize * i;
-                int y1 = tileSize * j;
+                int x1 = originX + tileSize * i;
+                int y1 = originY + tileSize * j;
                 int x2 = x1 + tileSize;
                 int y2 = y1 + tileSize;
                 if (!labyrinth.canMove(i, j, Labyrinth.Direction.RIGHT)) {
-                    c.drawLine(x1-wallOffset, y2, x2+wallOffset, y2, wallPaint);
+                    c.drawRect(x2-wallWidth, y1-wallWidth, x2, y2, wallPaint);
                 }
                 if (!labyrinth.canMove(i, j, Labyrinth.Direction.DOWN)) {
-                    c.drawLine(x2, y1-wallOffset, x2, y2+wallOffset, wallPaint);
+                    c.drawRect(x1-wallWidth, y2-wallWidth, x2, y2, wallPaint);
                 }
             }
         }
+        return image;
     }
 }

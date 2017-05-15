230cc8dd4141f8a7e98f6cf3d778b44a29a621ca
Never M <nevermournee@gmail.com>
2017-05-15 12:40:19 +0300

    Refactor tests: move data to static fields

--- app/src/test/java/com/domain/nvm/morningfriend/equation/ExpressionTest.java
+++ app/src/test/java/com/domain/nvm/morningfriend/equation/ExpressionTest.java
@@ -6,147 +6,149 @@ import com.domain.nvm.morningfriend.features.puzzle.equation.data.Negation;
 import com.domain.nvm.morningfriend.features.puzzle.equation.data.Sum;
 import com.domain.nvm.morningfriend.features.puzzle.equation.data.Variable;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ExpressionTest {
 
-    private Environment emptyEnv;
-    private Environment env;
+    private static Environment emptyEnv;
+    private static Environment env;
+    private static Constant const1, const2, const3;
+    private static Variable varX;
 
-    @Before
-    public void setUp() {
+    /*
+     * Since all our data is immutable we may as well
+     * initialize it once at class level for all tests
+     */
+    @BeforeClass
+    public static void setUp() {
         emptyEnv = new Environment();
+        varX = new Variable("x");
         env = new Environment();
-        env.set(new Variable("x"), 5);
+        env.set(varX, 5);
+        const1 = new Constant(1);
+        const2 = new Constant(2);
+        const3 = new Constant(3);
     }
 
     @Test
     public void isCompoundConstant() {
-        Constant x1 = new Constant(1);
-        assertFalse(x1.isCompound());
+        assertFalse(const1.isCompound());
     }
 
     @Test
     public void isCompoundVariable() {
-        Variable x = new Variable("x");
-        assertFalse(x.isCompound());
+        assertFalse(varX.isCompound());
     }
 
     @Test
     public void isCompoundSum() {
-        Sum s = new Sum(new Constant(2), new Constant(3));
+        Sum s = new Sum(const2, const3);
         assertTrue(s.isCompound());
     }
 
     @Test
     public void isCompoundNegation() {
-        Negation n = new Negation(new Constant(2));
+        Negation n = new Negation(const2);
         assertTrue(n.isCompound());
     }
 
     @Test
     public void evalConstant() {
-        Constant x1 = new Constant(1);
-        assertEquals(1, x1.eval(emptyEnv));
+        assertEquals(1, const1.eval(emptyEnv));
     }
 
 
     @Test(expected= Environment.UndefinedVariableException.class)
     public void evalVariableNotFound() throws Environment.UndefinedVariableException{
-        Variable x = new Variable("x");
-        int v = x.eval(emptyEnv);
+        int v = varX.eval(emptyEnv);
     }
 
     @Test
     public void evalVariable() throws Environment.UndefinedVariableException {
-        Variable x = new Variable("x");
-        assertEquals(5, x.eval(env));
+        assertEquals(5, varX.eval(env));
     }
 
     @Test
     public void evalSumOfConsts() throws Environment.UndefinedVariableException {
-        Sum s = new Sum(new Constant(2), new Constant(3));
+        Sum s = new Sum(const2, const3);
         assertEquals(5, s.eval(emptyEnv));
     }
 
     @Test
     public void evalSumOfVars() throws Environment.UndefinedVariableException {
-        Sum s = new Sum(new Variable("x"), new Variable("x"));
+        Sum s = new Sum(varX, varX);
         assertEquals(10, s.eval(env));
     }
 
     @Test
     public void evalSimpleNegation() throws Environment.UndefinedVariableException {
-        Negation n = new Negation(new Constant(3));
+        Negation n = new Negation(const3);
         assertEquals(-3, n.eval(emptyEnv));
     }
 
     @Test
     public void evalNegationOfSum() throws Environment.UndefinedVariableException {
-        Negation n = new Negation(new Sum(new Constant(2), new Constant(5)));
-        assertEquals(-7, n.eval(emptyEnv));
+        Negation n = new Negation(new Sum(const2, const3));
+        assertEquals(-5, n.eval(emptyEnv));
     }
 
     @Test
     public void evalSubtraction() throws Environment.UndefinedVariableException {
-        Sum s = Sum.makeSubtraction(new Constant(2), new Constant(5));
-        assertEquals(-3, s.eval(emptyEnv));
+        Sum s = Sum.makeSubtraction(const2, const3);
+        assertEquals(-1, s.eval(emptyEnv));
     }
 
     @Test
     public void variableGetVarname() {
-        Variable x = new Variable("x");
-        assertEquals("x", x.getVariableName());
+        assertEquals("x", varX.getVariableName());
     }
 
     @Test
     public void toStringConst() {
-        Constant a = new Constant(1);
-        assertEquals("1", a.toString());
+        assertEquals("1", const1.toString());
     }
 
     @Test
     public void toStringVariable() {
-        Variable x = new Variable("x");
-        assertEquals("x", x.toString());
+        assertEquals("x", varX.toString());
     }
 
     @Test
     public void toStringSumSimple() {
-        Sum s = new Sum(new Constant(2), new Constant(3));
+        Sum s = new Sum(const2, const3);
         assertEquals("2 + 3", s.toString());
     }
 
     @Test
     public void toStringSumWithVar() {
-        Sum s = new Sum(new Constant(2), new Variable("x"));
+        Sum s = new Sum(const2, varX);
         assertEquals("2 + x", s.toString());
     }
 
     @Test
     public void toStringSumCompound() {
-        Sum s = new Sum(new Constant(2), new Variable("x"));
+        Sum s = new Sum(const2, varX);
         assertEquals("2 + x + 2 + x", new Sum(s, s).toString());
     }
 
     @Test
     public void toStringNegationOfConstant() {
-        Negation n = new Negation(new Constant(1));
+        Negation n = new Negation(const1);
         assertEquals("-1", n.toString());
     }
 
     @Test
     public void toStringNegationOfVariable() {
-        Negation n = new Negation(new Variable("x"));
+        Negation n = new Negation(varX);
         assertEquals("-x", n.toString());
     }
 
     @Test
     public void toStringNegationOfSum() {
-        Negation n = new Negation(new Sum(new Constant(2), new Constant(3)));
+        Negation n = new Negation(new Sum(const2, const3));
         assertEquals("-(2 + 3)", n.toString());
     }
 

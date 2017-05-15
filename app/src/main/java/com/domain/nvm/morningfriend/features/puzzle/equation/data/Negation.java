26eb106d66230bd23f1104a70a98bd7a7d35c48c
Never M <nevermournee@gmail.com>
2017-05-15 12:04:38 +0300

    Implement Negation

--- app/src/main/java/com/domain/nvm/morningfriend/features/puzzle/equation/data/Negation.java
+++ app/src/main/java/com/domain/nvm/morningfriend/features/puzzle/equation/data/Negation.java
@@ -2,17 +2,34 @@ package com.domain.nvm.morningfriend.features.puzzle.equation.data;
 
 public class Negation implements Expression {
 
-    public Negation(Expression e) {
+    private Expression negand;
 
+    public Negation(Expression e) {
+        negand = e;
+    }
+
+    public Expression getNegand() {
+        return negand;
     }
 
     @Override
     public int eval(Environment env) throws Environment.UndefinedVariableException {
-        return 0;
+        return -1 * negand.eval(env);
     }
 
     @Override
     public boolean isCompound() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String negandStr = negand.toString();
+        if (negand.isCompound()) {
+            return "-(" + negandStr + ")";
+        }
+        else {
+            return "-" + negandStr;
+        }
     }
 }
